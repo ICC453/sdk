cb66dae1a2f6fb95c5026de787ef83720a59d4a8
Cesar Melo <cavmelo@gmail.com>
2019-11-08 12:33:40 -0400

    first commit

--- dist/common/environment.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    log: { url: 'http://localhost:3000' },
-};
-//# sourceMappingURL=environment.js.map
